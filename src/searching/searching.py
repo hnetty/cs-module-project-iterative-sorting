e2a26580399d71726fa5d2311dbd5f28ffec7da9
Harper Nettesheim <harpernettesheim@gmail.com>
2020-09-14 15:24:41 -0500

    MVP on the day

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,24 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
 
-    # Your code here
-
-
+    while left < right:
+        midpoint = (right + left) // 2
+        if arr[midpoint] == target:
+            return midpoint
+        elif arr[midpoint] > target:
+            right = midpoint - 1
+        else:
+            left = midpoint + 1
+    
     return -1  # not found
